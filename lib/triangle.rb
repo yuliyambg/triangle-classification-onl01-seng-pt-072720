54e80b01fb288475fd9713f4034168c6a65670d7
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-15 14:50:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -33,19 +33,18 @@ class Triangle
 
 
   def kind
-  min, min2, max = [@a, @b, @c].sort
-if min < 0 || min + min2 <= max
-raise TriangleError
-end
-  if @a == @b && @b == @c 
-:equilateral
-elsif @a == @b || @a == @c || @b == @c
-:isosceles
-else
-:scalene
-end
-
-end
+    min, min2, max = [@a, @b, @c].sort
+  if min < 0 || min + min2 <= max
+    raise TriangleError
+  end
+    if @a == @b && @b == @c 
+       :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+       :isosceles
+    else
+       :scalene
+    end
+  end
 
 
 class TriangleError < StandardError
