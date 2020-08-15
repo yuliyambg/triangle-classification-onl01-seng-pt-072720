d8b410ffc4385bb23f525f7b084adc5b226ac3d8
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-15 14:35:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -37,7 +37,7 @@ def kind
   min, min2, max = [@a, @b, @c].sort
 if min < 0 || min + min2 <= max
 raise TriangleError
-
+end
   if @a == @b && @b == c
 :equilateral
 elsif @a == @b || @a == @c || @b == @c
