a848950d5c26760120bce8e8fcea284af3b6238e
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-15 14:00:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
   # write code here
+  
+  
+  attr_accessor :side_a, :side_b, :side_c
+  
+  def initialize(side_a,side_b,side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
 end
