38cff45506b2565781660721eb7dd26c69e9ca20
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-15 14:38:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -38,7 +38,7 @@ def kind
 if min < 0 || min + min2 <= max
 raise TriangleError
 end
-  if @a == @b && @b == c
+  if @a == @b && @b == @c 
 :equilateral
 elsif @a == @b || @a == @c || @b == @c
 :isosceles
