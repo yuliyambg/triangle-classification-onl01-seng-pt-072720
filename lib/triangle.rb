27bbdf3f75ad186aab93d73c4b1abbf68ab881ec
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-15 14:30:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,26 +2,33 @@ class Triangle
   # write code here
   
   
-  attr_accessor :a, :b, :c
+  # attr_accessor :a, :b, :c
+  
+  # def initialize(a,b,c)
+  #   @a, @b, @c = [a, b, c].sort
+  #   raise TriangleError unless valid_triangle?
+  #   @kind = kind
+
+  # end
+
+  #   def valid_triangle?
+  #   @a > 0 && @a + @b > @c
+  # end
+
+
+  # def kind
+  # [:equilateral, :isosceles, :scalene][[@a, @b, @c].uniq.size - 1]
+  # end
+  
+  # class TriangleError < StandardError
+  #   # triangle error code
+  # end
+  
+  
+  attr_accessor :a, :b, :code
   
   def initialize(a,b,c)
-    @a, @b, @c = [a, b, c].sort
-    raise TriangleError unless valid_triangle?
-    @kind = kind
-
+    @a, @b, @c = a,b,c
   end
 
-    def valid_triangle?
-    @a > 0 && @a + @b > @c
-   end
-
-
-  def kind
-  [:equilateral, :isosceles, :scalene][[@a, @b, @c].uniq.size - 1]
-  end
-  
-  class TriangleError < StandardError
-    # triangle error code
-  end
-  
 end
